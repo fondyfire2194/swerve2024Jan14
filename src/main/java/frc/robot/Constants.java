26f8d43e1e96eb7026fe4edeaf7cf0d44cd7f137
MATTHEW_JANKE <jankemat000@fondystudents.org>
2024-01-14 14:45:43 -0600

    Fixed Gyro Issue

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,7 +17,7 @@ public final class Constants {
     public static final double stickDeadband = 0.1;
 
     public static final int pigeonID = 6;
-    public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
+    public static final boolean invertGyro = true; // Always ensure Gyro is CCW+ CW-
 
     /* Drivetrain Constants */
     public static final Measure<Distance> trackWidth = Inches.of(20.75);
